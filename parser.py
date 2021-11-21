016ba4b7d736977da063d1ec1ab4e21abdd1f12b
Aliaksei Tkachou <you@example.com>
2021-11-21 22:08:19 +0300

    some minor fixes and cleaning

--- parser.py
+++ parser.py
@@ -1,14 +1,6 @@
 from bs4 import BeautifulSoup
-# from lxml import html
 import requests
 from re import sub
-
-# import json
-# a = [[1, 2], [3, 4]]
-# print(json.dumps(a))
-# a = json.dumps(a)
-# print(type(a))
-# print(json.loads(a)[0])
 
 pre_link = 'http://slounik.org/search?'
 # // mode = 0
@@ -32,10 +24,17 @@ encyclopedia = ['pismienniki', 'reldz', 'ruchi', 'demap', 'nac', 'asvietniki', '
 
 dict_organizer =[belru_dicts, explain_dicts, lexical_dicts, encyclopedia]
 
-def generate_link(search, dict=[0], un=1):
+def generate_link(search, dict=None, un=1):
+    """
+    :param search: word to look for
+    :param dict: number of a pack of dictionaries to use
+    :param un: "u nazve" -- look for the word only in title
+    :return: link to search for a word in dictionaries
+    """
     dict_list = ''
-    for i in range(len(dict)):
-        dict_list += '|'.join(dict_organizer[i]) * dict[i]
+    if dict:
+        for i in range(len(dict)):
+            dict_list += '|'.join(dict_organizer[i]) * dict[i]
     post_link = 'search=' + search + '&dict=' + dict_list + '&un=' + str(un)
     # final = urllib.parse.quote(link, safe='')
     final = pre_link + post_link
@@ -43,8 +42,7 @@ def generate_link(search, dict=[0], un=1):
     print('- - - -- link:  ', final)
     return final
 
-def get_translations(word, dict=[0], un=1):
-    # print(dict)
+def get_translations(word, dict=None, un=1):
     final_link = generate_link(word, dict=dict, un=un)
     response = requests.get(final_link)
 
@@ -61,21 +59,18 @@ def get_translations(word, dict=[0], un=1):
     # print(encyclopedia)
 
     soup_object = BeautifulSoup(response.text, 'lxml')
-    # w = open('web.html', 'w')
-    # w.write(response.text)
-    # w.close()
     list_of_translations = []
     translations = soup_object.find_all('li', id='li_poszuk')
     # print(len(translations))
     # print(*translations, sep='\n'+'_'*20+'\n')
     for each in translations:
+        # print(each)
         # each = str(each).replace('"', "''")
         without_comments = sub('(<!--.*?-->)', '', str(each))
         without_tags = sub('(<sub>.*?</sub>)', '', without_comments)
         without_tags= sub('(<sup>.*?</sup>)', '', without_tags)
         res = without_tags.split('// ')
         source = BeautifulSoup(res[1], 'lxml')
-        # print(' --- -- - - - ', source)
         dict_name = source.html.body.a.attrs['title']
         dict_link = 'http://slounik.org' + source.html.body.a.attrs['href']
         source = '<a href="%s">%s</a>' % (dict_link, dict_name)
@@ -90,4 +85,46 @@ def get_translations(word, dict=[0], un=1):
     # list of lists kinda[translation | book, it was taken from]
 
 if __name__ == '__main__':
-    print(*get_translations('склад', dict=[2]), sep='\n')
+
+    tt = [
+        'пить',
+        'пишет',
+        'плакать',
+        'план',
+        'пластмасса',
+        'платить',
+        'плоский',
+        'плохо',
+        'плохой',
+        'площадь',
+        'плыть',
+        'по',
+        'победить',
+        'поверить',
+        'повторить',
+        'погибнуть',
+        'погода',
+        'под',
+        'поднять',
+        'подняться',
+        'подойти',
+        'подруга',
+        'подумать',
+        'подходить',
+        'поезд',
+        'поехать',
+        'пожалуйста',
+        'позади',
+        'позволить',
+        'позвонить',
+        'поздний',
+        'пойти',
+        'пока',
+        'показать',
+        'показаться',
+        'пол',
+        'поле',
+    ]
+
+    for i in tt:
+        print(*get_translations(i), sep='\n\n\n\n\n\n\n')
